94fa7c3dd962cc1b1b8df29ae385648adff13af7
Rob J Goedman <goedman@icloud.com>
2026-07-17 09:51:41 -0600

    Remove .DS_Store, changes to playgrounds

---
+++ Playgrounds.playground/Pages/02Lecture_01.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+import SwiftQiskitCore
+
+var qc0 = QuantumCircuit(qubits: 2)
+qc0
+
+qc0.h(0)
+qc0.cx(0, 1)
+
+let qc0State = qc0.run()
+print(qc0State.amplitudes.description)
+
+qc0.measure(shots: 1024)
+qc0State.probabilities
+print(qc0State.probabilities.description)
+
+//: [Next](@next)
---
+++ Playgrounds.playground/Pages/03Lecture_03.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Previous](@previous)
+
+import Foundation
+import SwiftQiskitCore
+
+var sv0 = StateVector(qubits: 2)
+sv0.probabilities
+
+//: [Next](@next)
